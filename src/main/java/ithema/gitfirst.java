91f378cd738986bb43acc0ab978a45180014bed5
123 <1072718444@qq.com>
2023-12-01 19:29:25 +0800

    third commit

--- src/main/java/ithema/gitfirst.java
+++ src/main/java/ithema/gitfirst.java
@@ -4,5 +4,6 @@ public class gitfirst {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git1");
+        System.out.println("hello git2");
     }
 }
